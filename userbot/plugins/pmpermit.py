96e8e343b4fbe4db791b2069e08068e4b41932ed
CatuserbotBR <96200272+CatuserbotBR@users.noreply.github.com>
2021-12-16 09:24:14 -0300

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -119,7 +119,7 @@ async def do_pm_permit_action(event, chat):  # sourcery no-metrics
             remwarns=remwarns,
         )
     elif gvarstatus("pmmenu") is None:
-        USER_BOT_NO_WARN = f"""__Oi__ {mention}__, Ainda não aprovei você para me enviar mensagens pessoais. 
+        USER_BOT_NO_WARN = f"""__Oi__ {mention}__, ainda não aprovei você para me enviar mensagens pessoais. 
 
 Você tem {warns}/{totalwarns} avisos até que você seja bloqueado pelo CatUserbot.
 
@@ -328,10 +328,10 @@ async def do_pm_chat_action(event, chat):
     except AttributeError:
         PMMESSAGE_CACHE = {}
     if str(chat.id) not in PM_WARNS:
-        text = """__Heyy! I am busy right now I already asked you to wait know. After my work finishes. \
-We can talk but not right know. Hope you understand.__
-__My master will respond when he/she comes back online, if he/she wants to.__
-**Please do not spam unless you wish to be blocked and reported.**"""
+        text = """__Ei, tenha um pouco de paciência. Meu mestre ainda não viu sua mensagem. \
+Meu mestre geralmente responde às pessoas.__
+__Meu mestre responderá quando ele/ela voltar online, se ele/ela quiser.__
+**Não envie spam, a menos que deseje ser bloqueado e denunciado.**"""
         await event.reply(text)
         PM_WARNS[str(chat.id)] = 1
         sql.del_collection("pmwarns")
@@ -508,8 +508,8 @@ async def on_plug_in_callback_query_handler(event):
         text = "Esse menu está disponível apenas para as pessoas que entram em contato com você."
         return await event.answer(text, cache_time=0, alert=True)
     text = """__OK. Sua solicitação foi registrada. Não faça spam na caixa de entrada do meu mestre agora. \
-My master is busy right now, When My master comes online he/she will check your message and ping you. \
-Then we can extend this conversation more but not right now.__"""
+Meu mestre está ocupado agora. Quando meu mestre estiver online, ele verificará sua mensagem e enviará uma resposta para você. \
+Então, podemos estender essa conversa mais, mas não agora.__"""
     sqllist.add_to_list("pmenquire", event.query.user_id)
     try:
         PM_WARNS = sql.get_collection("pmwarns").json
@@ -528,10 +528,10 @@ async def on_plug_in_callback_query_handler(event):
     if event.query.user_id == event.client.uid:
         text = "Esse menu está disponível apenas para as pessoas que entram em contato com você."
         return await event.answer(text, cache_time=0, alert=True)
-    text = """__Okay. I have notified my master about this. When he/she comes comes online\
- or when my master is free he/she will look into this chat and will ping you so we can have a friendly chat.__\
+    text = """__OK. Eu avisei meu mestre sobre isso. Quando ele ficar online\
+ ou quando meu mestre estiver livre ele irá olhar para este chat e irá enviar uma resposta para você para que possamos ter um conversa amigável.__\
 
-**But right now please do not spam unless you wish to get blocked.**"""
+**Mas agora, por favor, não envie spam, a menos que deseje ser bloqueado.**"""
     sqllist.add_to_list("pmrequest", event.query.user_id)
     try:
         PM_WARNS = sql.get_collection("pmwarns").json
@@ -550,8 +550,8 @@ async def on_plug_in_callback_query_handler(event):
     if event.query.user_id == event.client.uid:
         text = "Esse menu está disponível apenas para as pessoas que entram em contato com você."
         return await event.answer(text, cache_time=0, alert=True)
-    text = """__Yaa sure we can have a friendly chat but not right now. we can have this\
-some other time. Right now I am a little busy. when I come online and if I am free. I will ping you ,this is Damm sure.__"""
+    text = """__Sim, claro que podemos ter um bate-papo amigável, mas não agora. nós podemos ter isso\
+em alguma outra hora. No momento, estou um pouco ocupado. quando estou online e se estou livre. Vou mandar uma resposta para você, isso é uma maldita certeza__"""
     sqllist.add_to_list("pmchat", event.query.user_id)
     try:
         PM_WARNS = sql.get_collection("pmwarns").json
@@ -583,7 +583,7 @@ async def on_plug_in_callback_query_handler(event):
          \n░░░░░░░░░░░█░░█\
          \n░░░░░░░░░░░█░░█\
          \n░░░░░░░░░░░░▀▀`\
-         \n**Tão chato, esta não é sua casa. Vá se incomodar em outro lugar.\
+         \n**Tão chato, esta não é sua casa. Vá incomodar em outro lugar.\
          \n\nE este é o seu último aviso, se você enviar mais uma mensagem você será bloqueado automaticamente.**"
     sqllist.add_to_list("pmspam", event.query.user_id)
     try:
@@ -602,60 +602,60 @@ async def on_plug_in_callback_query_handler(event):
     pattern="pmguard (on|off)$",
     command=("pmguard", plugin_category),
     info={
-        "header": "To turn on or turn off pmpermit.",
+        "header": "Para ligar ou desligar o pmpermit.",
         "usage": "{tr}pmguard on/off",
     },
 )
 async def pmpermit_on(event):
-    "Turn on/off pmpermit."
+    "Para ligar ou desligar o pmpermit."
     input_str = event.pattern_match.group(1)
     if input_str == "on":
         if gvarstatus("pmpermit") is None:
             addgvar("pmpermit", "true")
             await edit_delete(
-                event, "__Pmpermit has been enabled for your account successfully.__"
+                event, "__Pmpermit foi habilitado para sua conta com sucesso.__"
             )
         else:
-            await edit_delete(event, "__Pmpermit is already enabled for your account__")
+            await edit_delete(event, "__Pmpermit já está habilitado para sua conta__")
     elif gvarstatus("pmpermit") is not None:
         delgvar("pmpermit")
         await edit_delete(
-            event, "__Pmpermit has been disabled for your account successfully__"
+            event, "__Pmpermit foi desativado para sua conta com sucesso__"
         )
     else:
-        await edit_delete(event, "__Pmpermit is already disabled for your account__")
+        await edit_delete(event, "__Pmpermit já está desabilitado para sua conta__")
 
 
 @catub.cat_cmd(
     pattern="pmmenu (on|off)$",
     command=("pmmenu", plugin_category),
     info={
-        "header": "To turn on or turn off pmmenu.",
+        "header": "Para ligar ou desligar o pmmenu.",
         "usage": "{tr}pmmenu on/off",
     },
 )
 async def pmpermit_on(event):
-    "Turn on/off pmmenu."
+    "Para ligar ou desligar o pmmenu."
     input_str = event.pattern_match.group(1)
     if input_str == "off":
         if gvarstatus("pmmenu") is None:
             addgvar("pmmenu", "false")
             await edit_delete(
                 event,
-                "__Pmpermit Menu has been disabled for your account successfully.__",
+                "__Pmpermit Menu foi desativado para sua conta com sucesso.__",
             )
         else:
             await edit_delete(
-                event, "__Pmpermit Menu is already disabled for your account__"
+                event, "__Pmpermit Menu já está desabilitado para sua conta__"
             )
     elif gvarstatus("pmmenu") is not None:
         delgvar("pmmenu")
         await edit_delete(
-            event, "__Pmpermit Menu has been enabled for your account successfully__"
+            event, "__Pmpermit Menu foi habilitado para sua conta com sucesso__"
         )
     else:
         await edit_delete(
-            event, "__Pmpermit Menu is already enabled for your account__"
+            event, "__Pmpermit Menu já está habilitado para sua conta__"
         )
 
 
@@ -663,19 +663,19 @@ async def pmpermit_on(event):
     pattern="(a|approve)(?:\s|$)([\s\S]*)",
     command=("approve", plugin_category),
     info={
-        "header": "To approve user to direct message you.",
+        "header": "Para aprovar o usuário para mensagem direta.",
         "usage": [
-            "{tr}a/approve <username/reply reason> in group",
-            "{tr}a/approve <reason> in pm",
+            "{tr}a/approve <usuário/resposta com reação> em grupo",
+            "{tr}a/approve <reação> no privado ",
         ],
     },
 )
 async def approve_p_m(event):  # sourcery no-metrics
-    "To approve user to pm"
+    "Para aprovar o usuário para enviar mensagem no privado"
     if gvarstatus("pmpermit") is None:
         return await edit_delete(
             event,
-            f"__Turn on pmpermit by doing __`{cmdhd}pmguard on` __for working of this plugin__",
+            f"__Ative o pmpermit fazendo __`{cmdhd}pmguard on` __para trabalhar com este plugin__",
         )
     if event.is_private:
         user = await event.get_chat()
@@ -685,7 +685,7 @@ async def approve_p_m(event):  # sourcery no-metrics
         if not user:
             return
     if not reason:
-        reason = "Not mentioned"
+        reason = "Não mencionado"
     try:
         PM_WARNS = sql.get_collection("pmwarns").json
     except AttributeError:
@@ -710,7 +710,7 @@ async def approve_p_m(event):  # sourcery no-metrics
             sqllist.rm_from_list("pmoptions", chat.id)
         await edit_delete(
             event,
-            f"__Approved to pm__ [{user.first_name}](tg://user?id={user.id})\n**Motivo :** __{reason}__",
+            f"__Aprovado para enviar mensagens privadas__ [{user.first_name}](tg://user?id={user.id})\n**Motivo :** __{reason}__",
         )
         try:
             PMMESSAGE_CACHE = sql.get_collection("pmmessagecache").json
@@ -731,7 +731,7 @@ async def approve_p_m(event):  # sourcery no-metrics
     else:
         await edit_delete(
             event,
-            f"[{user.first_name}](tg://user?id={user.id}) __is already in approved list__",
+            f"[{user.first_name}](tg://user?id={user.id}) __já está na lista de aprovados__",
         )
 
 
@@ -739,22 +739,22 @@ async def approve_p_m(event):  # sourcery no-metrics
     pattern="(da|disapprove)(?:\s|$)([\s\S]*)",
     command=("disapprove", plugin_category),
     info={
-        "header": "To disapprove user to direct message you.",
-        "note": "This command works only for approved users",
-        "options": {"all": "To disapprove all approved users"},
+        "header": "Para reprovar que o usuário envie uma mensagem direta a você.",
+        "note": "Este comando funciona apenas para usuários aprovados",
+        "options": {"all": "Para reprovar todos os usuários aprovados"},
         "usage": [
-            "{tr}da/disapprove <username/reply> in group",
-            "{tr}da/disapprove in pm",
-            "{tr}da/disapprove all - To disapprove all users.",
+            "{tr}da/disapprove <usuário/resposta> em grupo",
+            "{tr}da/disapprove no privado",
+            "{tr}da/disapprove all - Para reprovar todos os usuários.",
         ],
     },
 )
 async def disapprove_p_m(event):
-    "To disapprove user to direct message you."
+    "Para reprovar que o usuário envie uma mensagem direta a você."
     if gvarstatus("pmpermit") is None:
         return await edit_delete(
             event,
-            f"__Turn on pmpermit by doing __`{cmdhd}pmguard on` __for working of this plugin__",
+            f"__Ative o pmpermit fazendo __`{cmdhd}pmguard on` __para trabalhar com este plugin__",
         )
     if event.is_private:
         user = await event.get_chat()
@@ -769,7 +769,7 @@ async def disapprove_p_m(event):
     if reason == "all":
         pmpermit_sql.disapprove_all()
         return await edit_delete(
-            event, "__Ok! I have disapproved everyone successfully.__"
+            event, "__OK! Eu reprovei todos com sucesso.__"
         )
     if not reason:
         reason = "Não mencionado."
@@ -782,7 +782,7 @@ async def disapprove_p_m(event):
     else:
         await edit_delete(
             event,
-            f"[{user.first_name}](tg://user?id={user.id}) __is not yet approved__",
+            f"[{user.first_name}](tg://user?id={user.id}) __ainda não foi aprovado__",
         )
 
 
@@ -790,19 +790,19 @@ async def disapprove_p_m(event):
     pattern="block(?:\s|$)([\s\S]*)",
     command=("block", plugin_category),
     info={
-        "header": "To block user to direct message you.",
+        "header": "Para bloquear o usuário para enviar mensagem direta a você.",
         "usage": [
-            "{tr}block <username/reply reason> in group",
-            "{tr}block <reason> in pm",
+            "{tr}block <usuário/resposta com reação> em grupo",
+            "{tr}block <reação> no privado",
         ],
     },
 )
 async def block_p_m(event):
-    "To block user to direct message you."
+    "Para bloquear o usuário para enviar mensagem direta a você."
     if gvarstatus("pmpermit") is None:
         return await edit_delete(
             event,
-            f"__Turn on pmpermit by doing __`{cmdhd}pmguard on` __for working of this plugin__",
+            f"__Ative o pmpermit fazendo __`{cmdhd}pmguard on` __para trabalhar com este plugin__",
         )
     if event.is_private:
         user = await event.get_chat()
@@ -838,7 +838,7 @@ async def block_p_m(event):
     await event.client(functions.contacts.BlockRequest(user.id))
     await edit_delete(
         event,
-        f"[{user.first_name}](tg://user?id={user.id}) __is blocked, he can no longer personal message you.__\n**Motivo:** __{reason}__",
+        f"[{user.first_name}](tg://user?id={user.id}) __está bloqueado, ele não pode mais enviar mensagens pessoais para você.__\n**Motivo:** __{reason}__",
     )
 
 
@@ -846,19 +846,19 @@ async def block_p_m(event):
     pattern="unblock(?:\s|$)([\s\S]*)",
     command=("unblock", plugin_category),
     info={
-        "header": "To unblock a user.",
+        "header": "Para desbloquear um usuário.",
         "usage": [
-            "{tr}unblock <username/reply reason> in group",
-            "{tr}unblock <reason> in pm",
+            "{tr}unblock <usuário/resposta com reação> em grupo",
+            "{tr}unblock <reação> no privado",
         ],
     },
 )
 async def unblock_pm(event):
-    "To unblock a user."
+    "Para desbloquear um usuário."
     if gvarstatus("pmpermit") is None:
         return await edit_delete(
             event,
-            f"__Turn on pmpermit by doing __`{cmdhd}pmguard on` __for working of this plugin__",
+            f"__Ative o pmpermit fazendo __`{cmdhd}pmguard on` __para trabalhar com este plugin__",
         )
     if event.is_private:
         user = await event.get_chat()
@@ -871,7 +871,7 @@ async def unblock_pm(event):
         reason = "Não mencionado."
     await event.client(functions.contacts.UnblockRequest(user.id))
     await event.edit(
-        f"[{user.first_name}](tg://user?id={user.id}) __is unblocked he/she can personal message you from now on.__\n**Motivo:** __{reason}__"
+        f"[{user.first_name}](tg://user?id={user.id}) __está desbloqueado, ele/ela pode enviar mensagens pessoais para você de agora em diante.__\n**Motivo:** __{reason}__"
     )
 
 
@@ -879,29 +879,29 @@ async def unblock_pm(event):
     pattern="l(ist)?a(pproved)?$",
     command=("listapproved", plugin_category),
     info={
-        "header": "To see list of approved users.",
+        "header": "Para ver a lista de usuários aprovados.",
         "usage": [
             "{tr}listapproved",
         ],
     },
 )
 async def approve_p_m(event):
-    "To see list of approved users."
+    "Para ver a lista de usuários aprovados."
     if gvarstatus("pmpermit") is None:
         return await edit_delete(
             event,
-            f"__Turn on pmpermit by doing __`{cmdhd}pmguard on` __to work this plugin__",
+            f"__Ative o pmpermit fazendo __`{cmdhd}pmguard on` __para trabalhar com este plugin__",
         )
     approved_users = pmpermit_sql.get_all_approved()
-    APPROVED_PMs = "**Current Approved PMs**\n\n"
+    APPROVED_PMs = "**Aprovados para enviar mensagens privadas**\n\n"
     if len(approved_users) > 0:
         for user in approved_users:
             APPROVED_PMs += f"• 👤 {_format.mentionuser(user.first_name , user.user_id)}\n**ID:** `{user.user_id}`\n**UserName:** @{user.username}\n**Date: **__{user.date}__\n**Motivo: **__{user.reason}__\n\n"
     else:
-        APPROVED_PMs = "`You haven't approved anyone yet`"
+        APPROVED_PMs = "`Você ainda não aprovou ninguém`"
     await edit_or_reply(
         event,
         APPROVED_PMs,
-        file_name="approvedpms.txt",
-        caption="`Current Approved PMs`",
+        file_name="aprovadospms.txt",
+        caption="`Aprovados para enviar mensagens privadas`",
     )
