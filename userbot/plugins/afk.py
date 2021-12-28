a06943ecf7e2ecb2b1b0f6b93a131d1d5eb9a540
CatuserbotBR <96200272+CatuserbotBR@users.noreply.github.com>
2021-12-27 23:39:48 -0300

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -115,7 +115,7 @@ async def on_afk(event):  # sourcery no-metrics
         if AFK_.afk_type == "media":
             if AFK_.reason:
                 message_to_reply = (
-                    f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo :** __{AFK_.reason}__"
+                    f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo:** __{AFK_.reason}__"
                 )
             else:
                 message_to_reply = f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo:** __não mencionado__"
@@ -124,11 +124,11 @@ async def on_afk(event):  # sourcery no-metrics
         elif AFK_.afk_type == "text":
             if AFK_.msg_link and AFK_.reason:
                 message_to_reply = (
-                    f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo :** __{AFK_.reason}__"
+                    f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo:** __{AFK_.reason}__"
                 )
             elif AFK_.reason:
                 message_to_reply = (
-                    f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo :** __{AFK_.reason}__"
+                    f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo:** __{AFK_.reason}__"
                 )
             else:
                 message_to_reply = f"▸ **Oi, estou ausente a {endtime}\n▸ Motivo:** __não mencionado__"
