d1c0772f3133b83b2b9a2f63767ae422db576c3c
CatuserbotBR <96200272+CatuserbotBR@users.noreply.github.com>
2022-01-01 12:43:22 -0300

    Update utils.py

--- userbot/helpers/functions/utils.py
+++ userbot/helpers/functions/utils.py
@@ -25,7 +25,7 @@ async def get_readable_time(seconds: int) -> str:
     count = 0
     up_time = ""
     time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
+    time_suffix_list = ["s.", "m", "h", "d"]
     while count < 4:
         count += 1
         remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
