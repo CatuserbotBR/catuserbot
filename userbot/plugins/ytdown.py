acc3c792e751d9fd9641c15f465657edb6057c45
CatuserbotBR <96200272+CatuserbotBR@users.noreply.github.com>
2022-01-06 11:46:50 -0300

    Create ytdown.py

---
+++ userbot/plugins/ytdown.py
@@ -0,0 +1,99 @@
+#created By Mine is Zarox (https://t.me/IrisZarox)
+#thanx to @pony0boy for @youtubednbot
+
+import asyncio
+import requests
+from time import time
+from datetime import datetime
+
+from . import hmention
+from userbot import catub
+from ..helpers.utils import reply_id
+from ..helpers.functions import yt_search
+from ..core.managers import edit_delete, edit_or_reply
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+plugin_category = "misc"
+
+def is_url(link):
+    try:
+        response = requests.get(link)
+        url = "yes"
+    except requests.exceptions.MissingSchema as exception:
+        url = "no"
+    return url
+
+@catub.cat_cmd(
+    pattern="(iyt)(a)?(?:\s|$)([\s\S]*)",
+    command=("iyt", plugin_category),
+    info={
+        "header": "Para baixar vídeos/curtas do youtube instantaneamente",
+        "examples": [
+            "{tr}iyt <inquerir/link/responder a um link>",
+        ],
+        "note": "Você pode alterar as configurações de download aqui em @youtubednbot envie /settings e prosseguir",
+    },
+)
+async def _(zarox):
+    "For downloading yt video/shorts instantly"
+    chat = "@youtubednbot"
+    reply_to_id = await reply_id(zarox)
+    C = zarox.pattern_match.group(2)
+    B = zarox.pattern_match.group(3)
+    A = await zarox.get_reply_message()
+    if A and A.message and not B:
+        if "youtu" in A.message:
+            mine = A.message
+        else:
+            return await edit_or_reply(zarox, "`Eu não consigo ler mentes dando algo para pesquisar`")
+    elif B:
+        yt_str = is_url(B)
+        if yt_str == "yes" and "youtu" in B:
+            mine = B
+        else:
+            mine = await yt_search(str(B))
+    else:
+        return await edit_or_reply(zarox, "`Eu não consigo ler mentes, dar algo para pesquisar`")
+    await edit_or_reply(zarox, "**Baixando...**")
+    async with zarox.client.conversation(chat) as conv:
+        try:
+            #await zarox.client(functions.account.UpdateNotifySettingsRequest(peer=chat, settings=types.InputPeerNotifySettings(show_previews=False, silent=True,)))
+            try:
+                msg_start = await conv.send_message("/start")
+                response = await conv.get_response()
+                await zarox.client.send_read_acknowledge(conv.chat_id)
+            except TimeoutError:
+                return await edit_or_reply(zarox, "`Não foi possível baixar o vídeo. Tente mais tarde`")
+            start = datetime.now()
+            try:
+                if C:
+                    msg = await conv.send_message(f"/a {mine}", link_preview=True)
+                else:
+                    msg = await conv.send_message(mine, link_preview=True)
+                await asyncio.sleep(0.1)
+                video = await conv.get_response()
+            except TimeoutError:
+                await zarox.client.delete_messages(conv.chat_id, [msg.id])
+                if C:
+                    msg = await conv.send_message(f"/a {mine}", link_preview=True)
+                else:
+                    msg = await conv.send_message(mine, link_preview=True)
+                await asyncio.sleep(0.1)
+                video = await conv.get_response()
+            await zarox.client.send_read_acknowledge(conv.chat_id)
+        except YouBlockedUserError:
+            await edit_or_reply(zarox, "**Erro:** `desbloqueie @youtubednbot `e tente novamente!`")
+            return
+        await zarox.delete()
+        end = datetime.now()
+        ms = (end - start).seconds
+        caption = f"[{msg.media.webpage.title}]({mine})"
+        cat = await zarox.client.send_file(
+            zarox.chat_id,
+            video,
+            caption=caption,
+            reply_to=reply_to_id,
+        )
+    await zarox.client.delete_messages(
+        conv.chat_id, [msg_start.id, response.id, msg.id, video.id,]
+    )
