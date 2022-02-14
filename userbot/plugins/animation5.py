fc1b3b572e5891a994a3c2e319e7d73a2edb9d9b
CatuserbotBR <96200272+CatuserbotBR@users.noreply.github.com>
2022-02-14 00:55:49 -0300

    Update animation5.py

--- userbot/plugins/animation5.py
+++ userbot/plugins/animation5.py
@@ -123,7 +123,7 @@ async def _(event):
         "😧",
         "😡",
         "😢",
-        "__**[Good to see you guys....]**__(github.com/sandy1709/catuserbot)",
+        "__**[Good to see you guys....]**__(github.com/catuserbotbr/catuserbot)",
     ]
     for i in animation_ttl:
         await asyncio.sleep(animation_interval)
