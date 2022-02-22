7160c5f660779ae54af27f3eb1b00ae18f7cfcfa
CatuserbotBR <96200272+CatuserbotBR@users.noreply.github.com>
2022-02-22 03:17:38 -0300

    Update dist.py

--- userbot/plugins/dist.py
+++ userbot/plugins/dist.py
@@ -1,4 +1,5 @@
 import os
+import random
 from random import choice
 
 from telethon.errors.rpcerrorlist import YouBlockedUserError
@@ -16,10 +17,11 @@ plugin_category = "extra"
     pattern="dist($)",
     command=("dist", plugin_category),
     info={
-        "header": "Para distorcer a mídia respondida (sticker, vídeo e imagem)",
+        "header": "Para distorcer a mídia ou texto respondido (sticker, vídeo e imagem)",
         "usage": [
-            "{tr}dist <qualquer mídia>",
+            "{tr}dist <qualquer mídia ou texto>",
         ],
+        "notas": "Para áudio use o `{tr}dista`, para mais informações use `{tr}help dista`."
     },
 )
 async def _(event):
@@ -39,6 +41,9 @@ async def _(event):
                 await tgs(event)
             elif mediatype in ["Sticker"]:
                 await media(event, mediatype)
+       elif ded.text:
+            edited_text = await mock(ded.text)
+            await edit_or_reply(event, edited_text)     
         else:
             await edit_or_reply(
                 event,
@@ -52,6 +57,17 @@ async def _(event):
 # Here begins the end of world
 
 
+async def mock(message):
+    reply_text = []
+    for charac in message:
+        if charac.isalpha() and random.randint(0, 1):
+            to_app = charac.upper() if charac.islower() else charac.lower()
+            reply_text.append(to_app)
+        else:
+            reply_text.append(charac)
+    return "".join(reply_text)
+    
+    
 async def tgs(message):
     "Destroys animated sticker"
     reply = await message.get_reply_message()
@@ -102,7 +118,7 @@ async def media(event, mediatype):
                 event, "**Erro:**\nDesbloqueie @distortionerbot e tente novamente."
             )
 
-
+   
 @catub.cat_cmd(
     pattern="dista( -r|$)",
     command=("dista", plugin_category),
@@ -112,6 +128,7 @@ async def media(event, mediatype):
             "r": "Use a flag `-r` para versão estourada",
         },
         "usage": ["{tr}dista", "{tr}dista -r"],
+        "notas": "Para mídia ou texto use o `{tr}dist`, para mais informações use `{tr}help dist`."
     },
 )
 async def kill_mp3(event):
