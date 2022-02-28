8d6846769436d5cbd0764666583bc3406063e4f0
CatuserbotBR <96200272+CatuserbotBR@users.noreply.github.com>
2022-02-28 00:01:23 -0300

    Traduzido Dyno usage para Uso do dyno by Giovany

--- userbot/plugins/heroku.py
+++ userbot/plugins/heroku.py
@@ -128,7 +128,7 @@ async def variable(var):  # sourcery no-metrics
 )
 async def dyno_usage(dyno):
     """
-    Get your account Uso de dyno
+    Get your account Uso do dyno
     """
     if (HEROKU_APP_NAME is None) or (HEROKU_API_KEY is None):
         return await edit_delete(
