01cd3f3abce85de09c469a9a1229f9fadafb0c9b
CatUserbot BR <96200272+CatuserbotBR@users.noreply.github.com>
2022-03-12 15:40:31 -0300

    Update pressF.py

--- userbot/plugins/pressF.py
+++ userbot/plugins/pressF.py
@@ -1,7 +1,7 @@
 
 from userbot import catub
 
-from ..core.managers import edit_delete
+
 from ..helpers.utils import reply_id
 
 plugin_category = "extra"
@@ -19,13 +19,13 @@ plugin_category = "extra"
 )
 async def GayIfUChangeCredit(event):
     "Bullies the victim"
-    await event.delete()
     if event.fwd_from:
         return
     bot = "@FsInChatBot"
     hidetxt = event.pattern_match.group(1)
     reply_to_id = await reply_id(event)
     if not hidetxt:
-        return await edit_delete(event, "__How should I bulli without text.__")
+        hidetxt = ""
+    await evento.delete()
     results = await event.client.inline_query(bot, hidetxt)
     await results[0].click(event.chat_id, reply_to=reply_to_id)
